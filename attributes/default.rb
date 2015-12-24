5bd3c8cdb02041151479b2d09eb0b25e8a545bf4
Bikin Sendiri <arduino@bikinsendiri.web.id>
2015-12-24 09:25:23 +0700

    Mysql installed, need to fix root password. Don't merge to develop yet

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,5 @@ default['basic-lamp-lemp']['httpd-user'] = 'web_admin'
 default['basic-lamp-lemp']['httpd-group'] = 'web_admin'
 
 default['basic-lamp-lemp']['document_root'] = '/var/www/default/public_html'
+
+default['basic-lamp-lemp']['passwords']['secret_path'] = '/etc/chef/secret/mysql_secret'
---
+++ recipes/db-mysql.rb
@@ -0,0 +1,26 @@
+#
+# Cookbook Name:: basic-lamp-lemp
+# Recipe:: db-mysql
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
+
+# Configure the mysql2 Ruby gem.
+mysql2_chef_gem 'default' do
+  action :install
+end
+
+# Configure the MySQL client.
+mysql_client 'default' do
+  action :create
+end
+
+# Load the secrets file and the encrypted data bag item that holds the root password.
+password_secret = Chef::EncryptedDataBagItem.load_secret(node['basic-lamp-lemp']['passwords']['secret_path'])
+root_password_data_bag_item = Chef::EncryptedDataBagItem.load('passwords', 'mysql_root_password', password_secret)
+
+# Configure the MySQL service.
+mysql_service 'default' do
+  initial_root_password 'hastolavisto'
+  action [:create, :start]
+end
+
