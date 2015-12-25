06ef14e5d714d2633ef4c3b58d1d5cd0c3614541
Bikin Sendiri <arduino@bikinsendiri.web.id>
2015-12-25 08:30:06 +0700

    Initial commit for php-fpm

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,3 +4,5 @@ default['basic-lamp-lemp']['httpd-group'] = 'web_admin'
 default['basic-lamp-lemp']['document_root'] = '/var/www/default/public_html'
 
 default['basic-lamp-lemp']['passwords']['secret_path'] = '/etc/chef/secret/mysql_secret'
+default['basic-lamp-lemp']['php-fpm-user'] = 'fpm_user'
+default['basic-lamp-lemp']['php-fpm-group'] = 'fpm_group'
