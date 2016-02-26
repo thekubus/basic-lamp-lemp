d368da1110eae52ac24d062620ea033c16ae69d3
Ben Chandra <artist.io@outlook.com>
2016-02-26 09:36:11 +0000

    Optimize apache for low memory server 512MB

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,5 @@
 default['basic-lamp-lemp']['httpd-user'] = 'web_admin'
 default['basic-lamp-lemp']['httpd-group'] = 'web_admin'
 
+default['basic-lamp-lemp']['server_admin'] = 'artist.io@outlook.com'
 default['basic-lamp-lemp']['document_root'] = '/var/www/default/public_html'
